74769c23e9e3c6abe2928baf50fb03e76a7fab08
Gini <djinymail@yahoo.fr>
2022-01-24 10:46:19 +0100

    Update *.*

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,11 +1,24 @@
-const swiper = new Swiper('.swiper', {
-	loop: true,
-	autoplay: {
-		delay: 3000,
-	},
+// const swiper = new Swiper('.swiper', {
+// 	loop: true,
+// 	autoplay: {
+// 		delay: 3000,
+// 	},
 
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+// });
+var burger_menu = document.querySelector('.burger-btn');
+var menu = document.querySelector('.window-menu');
+var cross = document.querySelector('.cross-wind');
+
+burger_menu.addEventListener('click', function () {
+	menu.classList.toggle('active');
 });
+
+cross.addEventListener('click', function () {
+	menu.classList.toggle('active');
+});
+
+
